068549133b62ff5532aef100609301db92adefbd
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-06-05 13:28:24 -0400

    pseudo for tip calc

---
+++ ryan-howard/tip calculator.js
@@ -0,0 +1,31 @@
+// BEGIN
+// INIT 
+    // Init(calculate percentage of given input)
+        //Bill inputs
+
+//VARIABLES
+        //terribleTip
+        //poorTip
+        //goodTip
+        //greatTip
+        //excellentTip
+    //
+        //roundTip
+        //totalBill
+        //serviceQuality
+            //terribleService
+            //poorService
+            //goodService
+            //greatService
+            //excellentService
+
+//FUNCTIONS
+        // totalBill(number) {
+            // if (serviceQuality < goodService) {
+                //return poorTip(number)
+            // else (serviceQuality >= goodService) {
+                //return (goodTip, greatTip, excellentTip)
+            //}
+            //}
+        //}
+//END
