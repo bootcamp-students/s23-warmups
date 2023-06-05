4d09488b18d79e51e3f2bbbf43a13aed466a544c
Matthew Toffey <134286170+Mjtoffey@users.noreply.github.com>
2023-06-05 08:45:51 -0400

    Update array-plus-array.js

--- matthew-toffey/array-plus-array.js
+++ matthew-toffey/array-plus-array.js
@@ -1,1 +1,7 @@
-
+function arrayPlusArray(arr1, arr2) {
+  let newArr = arr1.concat(arr2);
+  let val = newArr.reduce(function(accumulator, currentValue){
+    return accumulator + currentValue;
+  });
+  return val;
+}
