43a663f0fa7faf34fa3d1bd9b4689dc50ab927dc
Shawn Vera <shawnvera@gmail.com>
2023-06-06 12:42:10 +0000

    added volume-of-cuboid.js

---
+++ vera-shawn/volume-of-cuboid.js
@@ -0,0 +1,7 @@
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+      // your code here
+      // value of the cuboid = length * width * height
+      return length * width * height
+    }
+  }
