b5425f3e43844a6e080878a23c83ae4c0473e5c9
Andie <134252065+AndieRowell@users.noreply.github.com>
2023-06-06 06:01:29 -0700

    Update and rename n-divisible-by-x-and-y to n-divisible-by-x-and-y.js

---
+++ andie-rowell/n-divisible-by-x-and-y.js
@@ -0,0 +1,8 @@
+function isDivisible(n, x, y) {
+  return (n % x === 0 && n % y === 0);
+}
+
+//pseudocode:
+//variables are n x y
+//using if else statement to check divisibility
+//must return true 
