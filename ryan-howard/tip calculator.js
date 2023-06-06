9e6ad146a2ecde658d2ac1aa93124d80c7ba98a8
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-06-06 08:38:14 -0400

    Update tip calculator.js

--- ryan-howard/tip calculator.js
+++ ryan-howard/tip calculator.js
@@ -29,3 +29,32 @@
             //}
         //}
 //END
+
+function calculateTip(amount, rating) {
+    var rate = rating.toLowerCase();
+    
+    if(rate === "excellent"){
+      return Math.ceil(amount*20/100);
+    }
+    else if(rate === "great"){
+      return Math.ceil(amount*15/100);
+    }
+    
+    else if(rate === "good"){
+      return Math.ceil(amount*10/100);
+    }
+    
+    else if(rate === "poor"){
+      return Math.ceil(amount*5/100);
+    }
+    
+    else if(rate === "terrible"){
+      return Math.ceil(amount*0/100);
+    }
+    }
+    
+    // the rating.toLowerCase() acts as a string calling the string in question to lower case.
+    // rate = rating.tolowercase is just saying the variable rate is rating, but lower case
+    // the first statement is if rate === excellent, return math.ceil (rounding up) to the 20% of the amount you spent.
+    // the else ifs are the same with the numbers designated from great(15) to terrible(0)
+    // 
