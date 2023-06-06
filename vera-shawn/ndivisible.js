3515260518b92cacb5fd6ce49da2c0916b32b3c6
Shawn Vera <shawnvera@gmail.com>
2023-06-06 12:45:29 +0000

    added ndivisible.js

---
+++ vera-shawn/ndivisible.js
@@ -0,0 +1,12 @@
+function isDivisible(n, x, y) {
+    // is n divisible by x and y
+    if (n % x === 0 && n % y === 0) {
+      return true
+      else 
+        return false
+    }
+    // input must be positive
+    
+    // input must be non-zero numbers
+    
+  }
