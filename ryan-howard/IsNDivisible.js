c468ba3859062ba586a5a7c5cc0e52cd61053ffe
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-06-06 08:17:42 -0400

    Create IsNDivisible.js

---
+++ ryan-howard/IsNDivisible.js
@@ -0,0 +1,9 @@
+function isDivisible(n, x, y) {
+    return (n%x == 0 && n%y ==0) ? true : false;
+  }
+  
+  // % checks if there is a remainder, if there is none, that means it is perfectly divisible. 
+  // the () ? true : false is a if else statement. the condition is that if it is zero, that means it is, else it is not divisible which would be false
+  // (3 is divisible by 3, but not by 4, therefore it is false)
+  // (12 is divisible by 3 and 4, making it true)
+  // etc
