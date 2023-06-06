e3cb87630977ac6b0fc8c8116501dfac02891148
Andrew Ross <134294344+andycamguy@users.noreply.github.com>
2023-06-06 08:56:44 -0400

    Update volume_cuboid.js

--- Andrew_Ross/volume_cuboid.js
+++ Andrew_Ross/volume_cuboid.js
@@ -1,11 +1,11 @@
-//hasn't passed tests yet 
  class Kata {
   // define variables 
   static getVolumeOfCuboid(length, width, height) {
     // calculate the variables
-     length*width*height;
+     return length*width*height;
     
   }
 }
-console.log(Kata.getVolumeOfCuboid(1,2,2));
+
 //output
+
