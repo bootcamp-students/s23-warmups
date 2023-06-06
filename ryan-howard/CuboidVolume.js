f7a3ff4c4a2ddf19296f023c227e1a34625fda46
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-06-06 08:24:46 -0400

    Create CuboidVolume.js

---
+++ ryan-howard/CuboidVolume.js
@@ -0,0 +1,11 @@
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+      // your code here
+      var vol=width*length*height;
+    return vol;
+    }
+  }
+
+  // function is getVolumeOfCuboid, gives parameters, length, width height
+  //variable created to calculate volume, l*w*h which is 'vol'
+  //return vol 
