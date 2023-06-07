0bb5af9d026f915d4e00f07c7f0e505cfea83e98
Andrew Ross <134294344+andycamguy@users.noreply.github.com>
2023-06-07 08:38:11 -0400

    Create what-between.js

---
+++ Andrew_Ross/what-between.js
@@ -0,0 +1,11 @@
+function between(a, b) {
+  // define the array to place all integers
+  //for loop of a < b a++, insert  an i variable to insert the integers
+   let arr =[];
+  for (i=a;i<=b;i++) {
+          arr.push(i);
+  //incrementing for loop to insert integers
+  // your code here
+      }
+        return arr;
+}
