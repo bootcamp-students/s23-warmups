99798675aaefa0cae54c1ea4e2438c827dac06b6
Matthew Toffey <134286170+Mjtoffey@users.noreply.github.com>
2023-06-07 08:12:43 -0400

    Create what-is-between.js

---
+++ matthew-toffey/what-is-between.js
@@ -0,0 +1,11 @@
+function between(a, b) {
+  var numbers = []
+  for (var i = a; i <= b; i++) {
+    numbers.push(i);
+  }
+  return numbers;
+}
+//create variable to hold integerds
+//create a loop that evaluates all numbers within array
+//allow evaluation to take over new array
+//return numbers
