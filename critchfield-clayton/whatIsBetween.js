607b5cf457d6858390da18b6cf87036290e778b5
Clayton Critchfield <82044721+DeveloperDadd@users.noreply.github.com>
2023-06-07 08:15:37 -0400

    Create whatIsBetween.js
    Day 3 Challenge 1 Kata

---
+++ critchfield-clayton/whatIsBetween.js
@@ -0,0 +1,14 @@
+/*Take two parameters
+    START AN EMPTY ARRAY
+      RETURN everything in an array between [a, b] using loop;
+        THEN INCREMENT value a
+          RETURN array
+            END */
+
+function between(a, b) {
+  let array = [];
+  for (a === ''; a <= b; a++) {
+    array.push(a);
+  }
+  return array;
+};
