8689fae5a9f45dc2bae873b23be93f9c891d0c47
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-06-07 08:18:14 -0400

    Create what-is-between.js

---
+++ brad-kerr/what-is-between.js
@@ -0,0 +1,13 @@
+// create a variable that is an empty array
+// create a loop to count from a to b
+// push the loop to the array
+// return the array
+
+function between(a, b) {
+  let array = [];
+  
+  for (a === " "; a<=b; a++){
+    array.push(a);
+  }
+  return array;
+}
