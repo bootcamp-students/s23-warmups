e1ef4f29393dfed16e5e7d6ee96a6699300e87d0
Parker S <parker.senter@gmail.com>
2023-06-08 12:25:21 +0000

    Added remove string spaces.

---
+++ parker-senter/remove-string-spaces.js
@@ -0,0 +1,14 @@
+function noSpace(x){
+    return x.replaceAll(' ','');
+  }
+  
+  /*
+  PESUDOCODE
+  VARS:
+  x = input for function
+  
+  FUNC:
+  function noSpace(x) {
+    return x.replaceAll(' ','');
+  }
+  */
