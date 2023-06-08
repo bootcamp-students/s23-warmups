4f5c25fc2e4913de3bf4ddcdfd50082e96c71dbc
Matthew Toffey <134286170+Mjtoffey@users.noreply.github.com>
2023-06-08 08:34:35 -0400

    Create century-from-year.js

---
+++ matthew-toffey/century-from-year.js
@@ -0,0 +1,8 @@
+function century(year) {
+  let currentCentury = 0; //declares variable
+  while (year > 0) {
+    year = year - 100; //loops through and deletes 100 years each time
+    currentCentury = currentCentury + 1; // adds one to century count each time it erases 100
+  }
+  return currentCentury; // returns count of how many times it erased 100
+}
