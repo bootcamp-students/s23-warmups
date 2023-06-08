771ff9db6419023b6c846581256bef7f7afe0c14
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-06-08 08:35:16 -0400

    Create RemoveStringSpaces.js

---
+++ ryan-howard/RemoveStringSpaces.js
@@ -0,0 +1,4 @@
+function noSpace(x){
+  return x.split(" ").join('') //If (" ") is used as separator, the string is split between these lines of letters in an array
+  // join('') concatenates them using an empty string as a separator so theyre all joined without any space
+}
