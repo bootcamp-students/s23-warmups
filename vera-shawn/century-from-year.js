2e6e88306f968ed407221a25ec0cde02078dff75
Shawn <134296052+shawnvera@users.noreply.github.com>
2023-06-08 08:16:04 -0400

    updated

---
+++ vera-shawn/century-from-year.js
@@ -0,0 +1,6 @@
+function century(year) {
+    // return the century that any given year falls within
+    // math.ceil to get the upper number and divide by 100 to get the century.
+    
+    return Math.ceil(year/100);
+  }
