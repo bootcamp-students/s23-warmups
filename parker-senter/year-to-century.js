73083c6eb3dffa16bc74f91f081adf5072fc3d8c
Parker S <parker.senter@gmail.com>
2023-06-08 12:17:17 +0000

    added year to century.

---
+++ parker-senter/year-to-century.js
@@ -0,0 +1,25 @@
+function century(year) {
+    let yearToCent = Math.ceil(year/100)
+    return yearToCent;
+  }
+  
+  /*
+  PUESDOCODE
+  VAR:
+  century - what century is it
+  year - what year is it
+  yearToCent - what century the year is in
+  
+  FUNC:
+  function century(year) {
+   yearToCent = Math.ceil(year/100)
+   return yearToCent;
+  }
+  */
+  
+  /*ANOTHER WAY SHAWN FIGURED OUT
+
+  function century(year) {
+    return Math.ceil(year/100);
+  }
+  */
