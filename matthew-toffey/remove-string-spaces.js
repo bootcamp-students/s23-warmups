41dbbd34208d8731a982eba7ddbd178503bbfa77
Matthew Toffey <134286170+Mjtoffey@users.noreply.github.com>
2023-06-08 08:33:17 -0400

    Create remove-string-spaces.js

---
+++ matthew-toffey/remove-string-spaces.js
@@ -0,0 +1,4 @@
+function noSpace(x){
+  const spacesRemoved = x.replaceAll(' ', '');//replaces spaces with no space as defined
+  return spacesRemoved;//returns updated string using replace all to adjust value
+}
