40c717bd18f84f3f89e2b15d2fc16a3307ac590e
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-06-08 08:17:30 -0400

    Create century-from-year.js

---
+++ brad-kerr/century-from-year.js
@@ -0,0 +1,8 @@
+// Math.ceil rounds up to the nearest integer
+// When dividing the year by 100 it will turn it into a decimal 
+// Math.ceil will round the decimal up to the answer
+
+function century(year) {
+  // Finish this :)
+  return Math.ceil(year / 100)
+}
