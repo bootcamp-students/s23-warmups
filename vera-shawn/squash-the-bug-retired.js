30bf19c1c9428a8182342acd19c8e6369e98b216
Shawn <134296052+shawnvera@users.noreply.github.com>
2023-06-12 08:33:36 -0400

    updated with solution

---
+++ vera-shawn/squash-the-bug-retired.js
@@ -0,0 +1,11 @@
+function findLongest(str) {
+  
+    var spl = str.split(" ");
+    var longest = 0;
+    
+    for (let i = 0; i < spl.length; i++) {
+      if (spl[i].length > longest) {
+        longest = spl[i].length;
+      }}
+      return longest;
+  }
