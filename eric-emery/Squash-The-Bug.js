c3c889cd41cb3b52f30e8dec7c339c3b82879bee
Eric Emery <134020573+EricDEmery@users.noreply.github.com>
2023-06-12 08:35:23 -0400

    Create Squash-The-Bug.js

---
+++ eric-emery/Squash-The-Bug.js
@@ -0,0 +1,11 @@
+function findLongest(str) { //moustache goes here
+  var spl = str.split(" "); //splits into arrays of strings
+  var longest = 0; //initial number to keep track?
+  for (var i = 0; i < spl.length; i++) {
+    if (spl[i].length > longest) { // parenthese to brackets since its arrays, checks to see if current word is greater than the longest value
+      longest = spl[i].length; //if it is longer, becomes new length
+    }
+    }
+    return longest;
+}
+//lots of parentheses instead of moustaches
