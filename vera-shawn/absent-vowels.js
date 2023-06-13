de7862374d41ad0b3daed39cb7579b3b9df64f4b
Shawn <134296052+shawnvera@users.noreply.github.com>
2023-06-13 09:46:05 -0400

    updated absent-vowels.js

---
+++ vera-shawn/absent-vowels.js
@@ -0,0 +1,18 @@
+//BEGIN
+//INIT - define variables - define what is x?
+function absentVowel(x){
+    let vowels = [a, e, i, o, u];
+      
+      // declare a vowel index.
+      
+      let vowelIndex = null;
+
+      // for of loop to find vowels of x
+      for (vowel of x) {
+      if (i = 0; i < x.length; i++) {
+        
+      }
+    }
+      // return index corresponding to the vowel array.
+      //END
+    }
