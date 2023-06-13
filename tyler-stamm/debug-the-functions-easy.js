50877fc9ec1c5c9f9a9a88f20f1c0ddf78a5b0a4
Tyler Stamm <121567944+JTSTAM02@users.noreply.github.com>
2023-06-13 08:17:39 -0400

    Create debug-the-functions-easy.js

---
+++ tyler-stamm/debug-the-functions-easy.js
@@ -0,0 +1,9 @@
+function multi(arr) {
+  return arr.reduce((a,b)=>a*b,1);
+}
+function add(arr) {
+  return arr.reduce((a,b)=>a+b,0);
+}
+function reverse(str) {
+  return str.split('').reverse().join('');
+}
