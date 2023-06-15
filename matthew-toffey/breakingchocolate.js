e047ff3b93383d364bb2154875b5c0dde1e863f2
Matthew Toffey <134286170+Mjtoffey@users.noreply.github.com>
2023-06-15 09:03:37 -0400

    Create breakingchocolate.js

---
+++ matthew-toffey/breakingchocolate.js
@@ -0,0 +1,6 @@
+function breakChocolate(n,m) {
+  if (n === 0 || m === 0 ) return 0
+  return n*m -1;
+}
+//n and m have same value
+//value should return one less than total value to represent squares
