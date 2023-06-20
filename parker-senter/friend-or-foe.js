352379bd48f4a1c8868a1ed9965d57f41058614e
Parker S <parker.senter@gmail.com>
2023-06-20 12:48:27 +0000

    added friend or foe.

---
+++ parker-senter/friend-or-foe.js
@@ -0,0 +1,12 @@
+function friend(friends){
+    return friends.filter(friends => friends.length === 4);
+}
+  
+  /* 
+  PSEUDOCODE
+  //take string of names
+  //figure out which names have 4 letters
+  //return names with 4 letters
+  
+  friends.filter(friends => friends.length === 4);
+  */
