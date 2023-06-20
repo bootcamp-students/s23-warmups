e790e00c61c81f23c3a075a7d4d9279b1c87dfe6
Matthew Toffey <134286170+Mjtoffey@users.noreply.github.com>
2023-06-20 08:30:14 -0400

    Create friend-or-foe.js

---
+++ matthew-toffey/friend-or-foe.js
@@ -0,0 +1,9 @@
+function friend(friends){
+  return friends.filter(friends => friends.length === 4);
+}
+
+//BEGIN
+//GOAL-to filter through arr and return only 4 letter names
+//use filter method to evaluate arr
+//return appropriate names
+//END
