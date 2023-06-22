301741e4c046d6cb7d113e33d04a98842554c004
Matthew Toffey <134286170+Mjtoffey@users.noreply.github.com>
2023-06-22 09:12:24 -0400

    Create twoToOne.js

---
+++ matthew-toffey/twoToOne.js
@@ -0,0 +1,10 @@
+let longest = (s1, s2) =>
+[...new Set(s1 + s2)].sort().join('')
+
+
+//BEGIN
+//INIT-split to form new arr
+  //Use sort method the return alphabetical order
+//Function-split 2 strings
+  //newArr.sort to return proper value
+//END
