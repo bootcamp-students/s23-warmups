3038b71ee5f75c734ef9a202235ea6b106cb3393
Matthew Toffey <134286170+Mjtoffey@users.noreply.github.com>
2023-06-27 08:35:20 -0400

    Create housenumber.js

---
+++ matthew-toffey/housenumber.js
@@ -0,0 +1,20 @@
+function houseNumbersSum(inputArray) {
+  let sum = 0
+  for (var i = 0; i <inputArray.length; i++) {
+    if (inputArray[i]===0) {
+    break;
+  }
+    sum += inputArray [i];
+}
+    return sum;
+}
+  
+
+//BEGIN
+//INIT set new var 
+//FNCTN for loop to loop through arr
+  //if statement to stop when index value is 0
+  //break to stop an further looping
+//add empty var to indexs from arr
+//return new sum
+//END
