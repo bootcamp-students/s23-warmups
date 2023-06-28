8a45b17b12cd1a39d5452b7cf0ebedd51b681f1f
Shawn <134296052+shawnvera@users.noreply.github.com>
2023-06-28 08:23:10 -0400

    updated

---
+++ vera-shawn/use-map-to-double-values-in-an-array.js
@@ -0,0 +1,12 @@
+// BEGIN
+// Given the array double each item in the array
+// Return to new array
+// END
+
+
+function double(array) {
+    // Use array.map() to return a new array with each value twice
+    // as large as the corresponding value in the passed in array.
+return array.map(item => item * 2)
+
+}
