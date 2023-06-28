77a84dff02372aa512907c4b4dd0fcec2b03447f
Matthew Toffey <134286170+Mjtoffey@users.noreply.github.com>
2023-06-28 08:25:42 -0400

    Create mapForDoubles.js

---
+++ matthew-toffey/mapForDoubles.js
@@ -0,0 +1,10 @@
+function double(array) {
+    // Use array.map() to return a new array with each value twice
+    // as large as the corresponding value in the passed in array.
+  const doubles = array.map((num) => num * 2);
+  return doubles;
+}
+//BEGIN
+//new var to hold output
+//FNCTN map function to multiply array values
+//END
