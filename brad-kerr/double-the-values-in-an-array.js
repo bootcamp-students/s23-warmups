1a2c3f2149c4ce039a1f0a1a7207fa8e5b5e9d46
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-06-28 08:27:30 -0400

    Create double-the-values-in-an-array.js

---
+++ brad-kerr/double-the-values-in-an-array.js
@@ -0,0 +1,7 @@
+//return: An array thats double the original arary
+//parameter: An array
+//Use the .map method to multiply everything by 2
+
+function double(array) {
+return array.map((answer) => answer * 2)
+}
