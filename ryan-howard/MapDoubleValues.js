a306f874e9f95086c0c7cce24e7de56ad91a63b0
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-06-28 08:06:22 -0400

    Create MapDoubleValues.js

---
+++ ryan-howard/MapDoubleValues.js
@@ -0,0 +1,6 @@
+function double(array) {
+    // Use array.map() to return a new array with each value twice
+    // as large as the corresponding value in the passed in array.
+  const doubles = array.map((num) => num * 2);
+  return doubles;
+}
