800e4e5be6df2050d24274c5e166997483c1edba
Tyler Stamm <121567944+JTSTAM02@users.noreply.github.com>
2023-07-06 08:03:41 -0400

    Create Use reduce() to calculate the sum of the values in an array (retired).js

---
+++ tyler-stamm/Use reduce() to calculate the sum of the values in an array (retired).js
@@ -0,0 +1,12 @@
+function sum(array) {
+// INIT
+  // return the sum of the values in the array
+  // use .reduce to add the array items together and return a new array
+  // declare new variable (integer) to host the sum of the original array
+  // create new variable that reduces using the first variable equal to 0
+  const newArr= 0;
+  const sumWithInitial = array.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  newArr);
+    return sumWithInitial;
+}
