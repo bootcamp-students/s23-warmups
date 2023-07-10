9c8a87461dfde3b88ea2f0d86129db0b48e652a9
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-07-10 08:15:02 -0400

    Create FindUniqueNumber.js

---
+++ ryan-howard/FindUniqueNumber.js
@@ -0,0 +1,14 @@
+function findUniq(arr) {
+return arr.find(
+(item) => arr.indexOf(item) === arr.lastIndexOf(item)
+);
+}
+
+
+//BEGIN
+//INIT
+//loop through an array
+//find an array method to identify a unique number in a set.
+//END
+
+//.find
