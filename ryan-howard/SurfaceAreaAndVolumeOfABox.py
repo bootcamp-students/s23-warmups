2339caf85edf0b2c8daad2c2d5f2bc19f980f4ef
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-07-11 08:23:32 -0400

    Create SurfaceAreaAndVolumeOfABox.py

---
+++ ryan-howard/SurfaceAreaAndVolumeOfABox.py
@@ -0,0 +1,12 @@
+def get_size(w,h,d):
+    # surface area= w times h times d
+    # volume= Surface Area = 2lw + 2lh + 2wh
+    # add empty array
+    # define surface area and volume
+    
+  result = [];
+  surfaceArea = (2 * (w * h)) + (2 * (w * d)) + (2 * (h * d));
+  volume = w * h * d;
+  result.append(surfaceArea);
+  result.append(volume);
+  return result;
