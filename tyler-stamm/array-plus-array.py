6cd8d059e7afb1f630af68ccd1529aa3e56b31c2
Tyler Stamm <121567944+JTSTAM02@users.noreply.github.com>
2023-07-12 08:19:45 -0400

    Create array-plus-array.py

---
+++ tyler-stamm/array-plus-array.py
@@ -0,0 +1,10 @@
+def array_plus_array(arr1,arr2):
+    # INIT
+    #new variable to store sum of all elements
+    # output number
+    # arr1 and arr2 are lists
+    # How do we combine all the elements of a list AND ADD THEM TOGETHER?
+    # use sum to combine them all together
+    return sum(arr1 + arr2)
+    
+    # END
