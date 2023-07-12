b1701cd88273fb9652cd685c7bcc96e8d13319b4
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-07-12 08:09:28 -0400

    Create array-plus-array.py

---
+++ brad-kerr/array-plus-array.py
@@ -0,0 +1,5 @@
+# Return an integer that adds up all the integers from both the lists
+# The sum method adds up all the integers in the list
+
+def array_plus_array(arr1,arr2):
+    return sum(arr1) + sum(arr2)
