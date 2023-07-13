846f4a68ee2ba771bd5485d4efd3fb2307ad4783
Matthew Toffey <134286170+Mjtoffey@users.noreply.github.com>
2023-07-13 08:35:51 -0400

    Create coinCalculator.py

---
+++ matthew-toffey/coinCalculator.py
@@ -0,0 +1,9 @@
+def coin_combo(cents):
+    return [cents % 5, cents % 25 % 10 // 5, cents % 25 // 10, cents // 25]
+   
+# BEGIN
+# INIT
+# FNCTNS
+    #use math to find appropriate values
+    #return appropriate values
+# END
