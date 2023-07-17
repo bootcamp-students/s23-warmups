1f1432c883dd6b896cfa239fd594015e7e2e2996
Matthew Toffey <134286170+Mjtoffey@users.noreply.github.com>
2023-07-17 08:14:01 -0400

    Create removeSpaces.py

---
+++ matthew-toffey/removeSpaces.py
@@ -0,0 +1,10 @@
+def no_space(x):
+        return x.replace(" ", "")
+    
+    
+    
+#BEGIN
+#Given a string with spaces 
+#replace spaces within string with "no spaces"
+#return string with no spaces
+#end
