5f55543881eb90a7776b847014f2651c785b51c4
Parker S <111610335+Psenter@users.noreply.github.com>
2023-07-17 08:15:00 -0400

    Create centuryFromYear.py

---
+++ parker-senter/centuryFromYear.py
@@ -0,0 +1,12 @@
+import math
+
+def century(year):
+    return math.ceil(year/100)
+
+"""
+PSEUDOCODE
+take in a year
+divide the year by 100
+round the number up ot nearest whole number
+return century
+"""
