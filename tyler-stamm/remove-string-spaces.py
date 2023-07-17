001c959f45aa935f03bff2efb6bd57b9ef539957
Tyler Stamm <121567944+JTSTAM02@users.noreply.github.com>
2023-07-17 08:44:42 -0400

    Create remove-string-spaces.py

---
+++ tyler-stamm/remove-string-spaces.py
@@ -0,0 +1,9 @@
+def no_space(x):
+    # remove spaces
+    # return new string
+    # replace space with nothing
+    return x.replace(' ', '')
+
+    # loop through x
+    # store x in a new variable called newX
+    # 
