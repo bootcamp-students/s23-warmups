667baf920c5cd4ff87b0d6efa5cb4776cc3fe4bf
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-07-17 08:26:19 -0400

    Create RemoveStringSpaces.py

---
+++ ryan-howard/RemoveStringSpaces.py
@@ -0,0 +1,12 @@
+def no_space(x):
+    text= ("".join(x.split(' ')))
+    return text
+    
+#Split each individual string and get rid of spaces using join
+
+# #def no_space(x):
+#     result = ""
+#     for letters in x:
+#         if letters != ' ':
+#             result += letters
+#     return result
