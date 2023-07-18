1828648be4fd94d76f87369d8d02636641bfe688
Matthew Toffey <134286170+Mjtoffey@users.noreply.github.com>
2023-07-18 08:35:00 -0400

    Create lengthSelectLike.sql

---
+++ matthew-toffey/lengthSelectLike.sql
@@ -0,0 +1,8 @@
+-- Replace with your SQL Query
+SELECT first_name, last_name /* selects data to be displayed from table*/
+FROM names/*pulls from data sheet*/
+WHERE first_name LIKE '%______%'/*sorts the first name data tpye but number of charcters (6)*/
+/*BEGIN
+given a table with data
+return column of first name == 6 characters 
+*/
