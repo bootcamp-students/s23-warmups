1ee0a3d575dfc73831c30a10315ab083aae6e203
Matthew Toffey <134286170+Mjtoffey@users.noreply.github.com>
2023-07-18 08:21:33 -0400

    Create easyOrdering.sql

---
+++ matthew-toffey/easyOrdering.sql
@@ -0,0 +1,9 @@
+/*  SQL  */
+/* BEGIN
+given data in sorted fashion 
+must data in table with data sorted into highest - lowest
+return sorted
+END */
+
+SELECT * FROM companies
+ORDER BY employees DESC
