74847811909d1f0de8e4ac29c18b9786470a05e8
Matthew Toffey <134286170+Mjtoffey@users.noreply.github.com>
2023-07-19 08:58:58 -0400

    Create asciiConverter.sql

---
+++ matthew-toffey/asciiConverter.sql
@@ -0,0 +1,8 @@
+/*  SQL  */
+SELECT id, ascii(name) AS name, birthday, ascii(race) AS race--ascii will display () as ascii value
+FROM demographics
+
+--BEGIN
+--given table of demographics
+--return table but using ascii values 
+--END
