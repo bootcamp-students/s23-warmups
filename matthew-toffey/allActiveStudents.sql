cc22c037867b2ea8ecd6c7d885bf30f5b192d862
Matthew Toffey <134286170+Mjtoffey@users.noreply.github.com>
2023-07-19 08:33:00 -0400

    Create allActiveStudents.sql

---
+++ matthew-toffey/allActiveStudents.sql
@@ -0,0 +1,10 @@
+-- Type your query here
+SELECT * 
+FROM students 
+WHERE isActive = 1 --true, false only diplays as str so use bit type operator instead (1,0)
+
+--BEGIN
+--givent able of all students 
+--new tbale should only display active students
+--sort by boolean value and retunr table where isActive is true
+--END
