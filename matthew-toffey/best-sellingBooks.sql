a6acb18209fe4f7dcd4c1ad0f9b3b93673e501cc
Matthew Toffey <134286170+Mjtoffey@users.noreply.github.com>
2023-07-19 08:43:37 -0400

    Create best-sellingBooks.sql

---
+++ matthew-toffey/best-sellingBooks.sql
@@ -0,0 +1,11 @@
+-- Your Code Here
+SELECT *
+FROM books 
+ORDER BY copies_sold DESC LIMIT 5; --limit 5 selects only the first 5 results from orderby statement
+
+--BEGIN
+--given table of books and copies sold
+--must sort them by most sold
+--sort by desc, biggest numbers first
+--limit to top 5
+--END
