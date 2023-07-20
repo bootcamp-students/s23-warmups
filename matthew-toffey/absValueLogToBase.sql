a3b53410bf9adc629f0f090c7cabec9e148585b5
Matthew Toffey <134286170+Mjtoffey@users.noreply.github.com>
2023-07-20 08:56:31 -0400

    Create absValueLogToBase.sql

---
+++ matthew-toffey/absValueLogToBase.sql
@@ -0,0 +1,8 @@
+/*  SQL  */
+SELECT ABS(number1) as abs, LOG(64, number2) as log 
+FROM decimals
+
+--BEGIN
+--Given a table of decimals
+--must change the presentation of sumbers using LOG and ABS values in new columns abs and log
+--end
