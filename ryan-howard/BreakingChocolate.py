61ec6982b4feff127dcc8f505764654f5c817107
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-07-24 08:17:38 -0400

    Create BreakingChocolate.py

---
+++ ryan-howard/BreakingChocolate.py
@@ -0,0 +1,14 @@
+def break_chocolate(n, m):
+    return n * m - 1 if n and m else 0
+
+
+# def break_chocolate(n, m):
+#     if ((n>1 and m>1) or (n>1 and m==1) or (n==1 and m>1)):
+#         return n * m - 1;
+#     else:
+#         return 0;
+
+
+# Take in 2 numbers
+# Check whether the numbers are one or Above or Zero
+#Multiple the numbers and subtract one bc the last square doesnt needed to be taken off
