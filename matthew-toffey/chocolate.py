50a61f9392351fd31c2d1b071465bf10a19da49a
Matthew Toffey <134286170+Mjtoffey@users.noreply.github.com>
2023-07-24 08:09:17 -0400

    Create chocolate.py

---
+++ matthew-toffey/chocolate.py
@@ -0,0 +1,8 @@
+def break_chocolate(n, m):
+    return max(n * m -1, 0)
+
+#BEGIN
+#GIVEN a bar of chocolate 
+#Must break chocolate into individual pieces from total bar amount
+#Evaluation must take into account all breaks needed.  
+#Return number of evalutaion minus 1 
