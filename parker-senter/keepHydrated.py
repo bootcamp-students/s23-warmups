b80bb4fbc4e784b74b61fc1e054edf1d92d57a05
Parker S <111610335+Psenter@users.noreply.github.com>
2023-07-25 08:35:32 -0400

    Create keepHydrated.py

---
+++ parker-senter/keepHydrated.py
@@ -0,0 +1,11 @@
+import math
+def litres(time):
+    #multiplies time by .5 and rounds down
+    hydration = math.floor(time * .5)
+    return hydration
+
+"""
+PSEUDOCODE
+Multiply time which is hourly by .5
+Round down
+"""
