eb8a94379627d398a3c55953d0f731062d8bc751
Andrew Ross <134294344+andycamguy@users.noreply.github.com>
2023-07-27 08:33:57 -0400

    Create phone-number.py

---
+++ Andrew_Ross/phone-number.py
@@ -0,0 +1,9 @@
+# Thanks Matt for the help
+def create_phone_number(n):
+    #your code here
+    return "({0}{1}{2}) {3}{4}{5}-{6}{7}{8}{9}".format(*n)
+    #begin
+    #given list of 10 numbers
+    #must be returned in traditional phone number format
+    #place numbers by index
+    #return proper format
