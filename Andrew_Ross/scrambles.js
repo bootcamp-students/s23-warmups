4023809dd8b02e653ed35f98918fef04a4adf764
Andrew Ross <134294344+andycamguy@users.noreply.github.com>
2023-08-03 08:29:35 -0400

    Create scrambles.js

---
+++ Andrew_Ross/scrambles.js
@@ -0,0 +1,21 @@
+function scramble(str1, str2) {
+  const charCount = {};
+
+  // Count occurrences of characters in str1
+  for (let i = 0; i < str1.length; i++) {
+    const char = str1[i];
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
+
+  // Check if all characters in str2 are present in str1 with equal or greater counts
+  for (let i = 0; i < str2.length; i++) {
+    const char = str2[i];
+    if (!charCount[char] || charCount[char] === 0) {
+      return false;
+    } else {
+      charCount[char] -= 1;
+    }
+  }
+
+  return true;
+}
